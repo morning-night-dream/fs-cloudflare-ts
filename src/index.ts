0ec806581c4acf404a4fb838c1833102e2a49d56
takokun <takokun.778@gmail.com>
2022-11-26 15:13:40 +0900

    refactor

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,24 @@
-import { SlackChallengeRequest } from "./slack";
+import { challenge, SlackEvent } from "./slack";
 
-export interface Env {
+export type Env = {
 	API_KEY: string;
 	SLACK_SIGNING_SECRET: string;
-}
+};
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
 		console.debug(JSON.stringify(ctx));
 		// @ref https://developers.cloudflare.com/workers/examples/read-post/
 		const req = await request.json();
+
 		console.log(JSON.stringify(req));
-		const event: SlackChallengeRequest = JSON.parse(JSON.stringify(req));
-		const init = {
-			headers: {
-				"content-type": "text",
-			},
-			body: event.challenge,
-		};
-		return new Response(JSON.stringify(init));
+
+		const event: SlackEvent = JSON.parse(JSON.stringify(req));
+
+		if (event.type === "url_verification") {
+			return challenge(event);
+		}
+
+		return new Response(JSON.stringify(""));
 	},
 };
--- src/slack.ts
+++ src/slack.ts
@@ -1,7 +1,19 @@
-export interface SlackChallengeRequest {
+export type SlackEvent = {
 	token: string;
 	challenge: string;
-	type: string;
-}
+	type: SlackEventType;
+};
+
+export type SlackEventType = "url_verification" | "";
+
+export const challenge = (event: SlackEvent) => {
+	const init = {
+		headers: {
+			"content-type": "text",
+		},
+		body: event.challenge,
+	};
+	return new Response(JSON.stringify(event.challenge));
+};
 
 // export const verify = (header: string, body: string, secret: string) => {}
